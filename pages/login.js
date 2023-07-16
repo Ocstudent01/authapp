db3ff98d64fa717a1d2fc1bf4e7fdb3f97fedcc4
orlando <orlandch2023@gmail.com>
2023-07-16 02:40:19 -0500

    version 1

---
+++ pages/login.js
@@ -0,0 +1,14 @@
+import Head from "next/head"
+
+
+export default function Login(){
+    return (
+        <div>
+            <Head>
+                <h1>Login</h1>
+            </Head>
+
+            <h1>Login</h1>
+        </div>
+    )
+}
---
+++ pages/register.js
@@ -0,0 +1,13 @@
+import Head from "next/head"
+
+export default function Register(){
+    return (
+        <div>
+            <Head>
+                <h1>Register</h1>
+            </Head>
+
+            <h1>Register</h1>
+        </div>
+    )
+}
